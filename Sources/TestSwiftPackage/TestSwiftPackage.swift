45d9f82c9edf930519cd17b20f066c70ebd6bed9
Jon Osborne <jonosborne5@me.com>
2021-08-15 00:22:04 -0400

    added more public

--- Sources/TestSwiftPackage/TestSwiftPackage.swift
+++ Sources/TestSwiftPackage/TestSwiftPackage.swift
@@ -1,9 +1,21 @@
 public struct TestSwiftPackage {
     var text = "Hello, World!"
+    
+    public init(){
+        self.text = "Hello, World!"
+    }
+    
+    public func getText()->String{
+        return self.text
+    }
 }
 
 public struct AnotherStruct {
     public var text = "another struct"
+    
+    public init(){
+        
+    }
     
     public func returnSomeText()->String {
         return "this is some text and \(self.text)"
